b6f444ce422aeba4b199ff329d514b7f5675dc7c
viktor-v-i <viktor.mailmailv@gmail.com>
2026-03-29 10:46:10 +0200

    Revert "Reapply "Add bank-agent: Nordea CSV accountant with AI tagging""
    This reverts commit c7e374bfb5da6d983d14f11e8903d8b2adb6fc8e.

--- bank/bot.js
+++
@@ -1,7 +0,0 @@
-import TelegramBot from 'node-telegram-bot-api';
-
-if (!process.env.BANK_TELEGRAM_BOT_TOKEN) {
-  throw new Error('BANK_TELEGRAM_BOT_TOKEN is not set');
-}
-
-export const bot = new TelegramBot(process.env.BANK_TELEGRAM_BOT_TOKEN, { polling: true });
